b8d8640ef3f913bcb2160af71e21cf908a6b80f1
Rizal <43487664+rizalarifnugraha@users.noreply.github.com>
2018-10-28 19:27:44 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117048/latihan60/akatsuki/Akatsuki.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117048.latihan60.akatsuki;
+
+/**
+ *
+ * @author acer
+ */
+public class Akatsuki {
+
+    protected String pakaian;
+    protected String tujuan;
+
+    public Akatsuki(String pakaian, String tujuan) {
+        this.pakaian = pakaian;
+        this.tujuan = tujuan;
+    }
+
+    public String getpakaian() {
+        return pakaian;
+    }
+
+    public String gettujuan() {
+        return tujuan;
+    }
+
+}
